9ea9330970d609fa27a5ffc1c26b6d86cab6e98e
AboodXD <aboood40091@gmail.com>
2023-06-19 15:03:17 +0400

    res/ResFile: Implement IsValid()

--- src/nw/g3d/res/g3d_ResFile.cpp
+++ src/nw/g3d/res/g3d_ResFile.cpp
@@ -7,6 +7,82 @@
 
 namespace nw { namespace g3d { namespace res {
 
+namespace {
+
+#if !defined(NW_RELEASE)
+
+inline
+const char* GetFileName(const void* ptr)
+{
+    const ResFileData* pData = static_cast<const ResFileData*>(ptr);
+    if (pData->fileHeader.byteOrder == BinaryFileHeader::BYTE_ORDER_MARK)
+    {
+        return pData->ofsName.to_ptr();
+    }
+    else
+    {
+        const u32* ofsName = reinterpret_cast<const u32*>(&pData->ofsName);
+        return reinterpret_cast<const char*>(ofsName) + LoadRevU32(ofsName);
+    }
+}
+
+#endif
+
+}
+
+bool ResFile::IsValid(const void* ptr)
+{
+    NW_G3D_ASSERT_NOT_NULL(ptr);
+    const BinaryFileHeader* pHeader = static_cast<const BinaryFileHeader*>(ptr);
+
+    if (pHeader->sigWord != SIGNATURE)
+    {
+        NW_G3D_WARNING(false, "Signature check failed ('%c%c%c%c' must be '%c%c%c%c').\n",
+            pHeader->signature[0],
+            pHeader->signature[1],
+            pHeader->signature[2],
+            pHeader->signature[3],
+            NW_G3D_GET_SIGNATURE0(SIGNATURE),
+            NW_G3D_GET_SIGNATURE1(SIGNATURE),
+            NW_G3D_GET_SIGNATURE2(SIGNATURE),
+            NW_G3D_GET_SIGNATURE3(SIGNATURE)
+        );
+        return false;
+    }
+
+    if (pHeader->verWord != NW_G3D_VERSION)
+    {
+        NW_G3D_WARNING(false, "Version check failed (bin:'%d.%d.%d.%d', lib:'%d.%d.%d.%d').\n"
+            "\t[ResFile] %s\n",
+            pHeader->version[0],
+            pHeader->version[1],
+            pHeader->version[2],
+            pHeader->version[3],
+            NW_G3D_VERSION_MAJOR,
+            NW_G3D_VERSION_MINOR,
+            NW_G3D_VERSION_MICRO,
+            NW_G3D_VERSION_BINARYBUGFIX,
+            GetFileName(ptr)
+        );
+        return false;
+    }
+
+#if !NW_G3D_IS_HOST_WIN
+    if (pHeader->byteOrder != BinaryFileHeader::BYTE_ORDER_MARK)
+    {
+        NW_G3D_WARNING(false, "Endian check failed ('0x%X' must be '0x%X').\n"
+            "\t[ResFile] %s\n",
+            pHeader->byteOrder,
+            BinaryFileHeader::BYTE_ORDER_MARK,
+            GetFileName(ptr)
+        );
+        return false;
+    }
+#endif
+
+    return true;
+}
+
 ResFile* ResFile::ResCast(void* ptr)
 {
     NW_G3D_ASSERT_NOT_NULL(ptr);
