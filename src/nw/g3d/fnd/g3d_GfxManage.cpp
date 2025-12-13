0be5212ba209240a00b4281392d28be4706dcd6a
AboodXD <aboood40091@gmail.com>
2025-12-13 12:37:48 +0400

    fnd/GfxManage: Remove unused GPUClock & CPUClock

--- src/nw/g3d/fnd/g3d_GfxManage.cpp
+++ src/nw/g3d/fnd/g3d_GfxManage.cpp
@@ -1,39 +1,14 @@
 ﻿#include <nw/g3d/fnd/g3d_GfxManage.h>
 
-#include <algorithm>
 #include <nw/g3d/ut/g3d_Inlines.h>
 #include <nw/g3d/fnd/g3d_GfxState.h>
 #include <nw/g3d/fnd/g3d_GfxObject.h>
 
-#if NW_G3D_IS_HOST_WIN
-    #define WIN32_LEAN_AND_MEAN
-    #ifndef NOMINMAX
-        #define NOMINMAX
-    #endif // NOMINMAX
-    #include <windows.h>
-#elif NW_G3D_IS_HOST_CAFE
-    #include <cafe/os/OSTime.h>
-#endif
+#if NW_G3D_IS_HOST_CAFE
+    #include <cafe/os/OSCache.h>
+#endif // NW_G3D_IS_HOST_CAFE
 
 namespace nw { namespace g3d { namespace fnd {
-
-#if NW_G3D_IS_GX2
-GPUClock::Tick GPUClock::s_Freq = OS_TIMER_CLOCK;
-#elif NW_G3D_IS_GL
-GPUClock::Tick GPUClock::s_Freq = 1000 * 1000 * 1000;
-#endif
-
-CPUClock::Freq CPUClock::s_Freq;
-
-void CPUClock::Freq::Init()
-{
-#if NW_G3D_IS_GX2
-    m_Value = OS_TIMER_CLOCK;
-#elif NW_G3D_IS_GL && !defined( NW_STRIP_GL )
-    [[maybe_unused]] BOOL success = QueryPerformanceFrequency(reinterpret_cast<LARGE_INTEGER*>(&m_Value));
-    NW_G3D_ASSERT(success);
-#endif
-}
 
 bool CPUCache::IsValid(const void* addr, size_t size)
 {
